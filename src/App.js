139b0cc0e72b908e898d7da3d81b5b460a861807
sai-sreenath <sai-sreenath@users.noreply.github.com>
2020-04-15 18:27:56 -0400

    Method references in components -2

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
         <div className="App">
           <h1>Hi, I'm a React app</h1>
           <p>This is really working</p>
-          <button onClick={this.switchNameHandler.bind(this, 'Maxmilian')}>Switch Name</button>
+          <button onClick={ () => this.switchNameHandler('Maxmilian')}>Switch Name</button>
           <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
           <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this,'Max!')} />
           <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
