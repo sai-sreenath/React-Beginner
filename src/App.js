36ba5a75e42be648f441e9eb62389c6aeb32b445
sai-sreenath <sai-sreenath@users.noreply.github.com>
2020-04-15 18:22:26 -0400

    Method references in components method-1

--- src/App.js
+++ src/App.js
@@ -1,46 +1,44 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
 
-const App = props => {
+class App extends Component {
     
-  const [personsState, setPersonsState] = useState({
+  state = {
       persons: [
         {name: 'Max', age:28},
         {name: 'Manu', age:29},
         {name: 'Stephanie', age:26}
       ],
       otherState: 'Some other value'
-  });
-
-  const [otherState,setOtherState] = useState('Some Other Value');
-
-  const switchNameHandler = () => {
-    //Shouldnt do this: this.state.persons[0].name='Maxmillan';
-    setPersonsState({
-      persons: [
-        {name: 'Maxmillan', age:28},
-        {name: 'Manu', age:29},
-        {name: 'Stephanie', age:27}
-      ],
-     // otherState: personsState.otherState
-    })
-  };
-  
-  return (
-      <div className="App">
-        <h1>Hi, I'm a React app</h1>
-        <p>This is really working</p>
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
-        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
-      </div>
-    );
   }
 
-
+  switchNameHandler = (newName) => {
+    //Shouldnt do this: this.state.persons[0].name='Maxmillan';
+    this.setState({
+      persons: [
+        {name: newName, age:28},
+        {name: 'Manu', age:29},
+        {name: 'Stephanie', age:27}
+      ]
+    })
+  }
+  
+  render(){
+      return (
+        <div className="App">
+          <h1>Hi, I'm a React app</h1>
+          <p>This is really working</p>
+          <button onClick={this.switchNameHandler.bind(this, 'Maxmilian')}>Switch Name</button>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this,'Max!')} />
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        </div>
+      ); 
+    }
+  } 
+ 
 export default App;
 
 
