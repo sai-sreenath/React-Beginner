6261ddd73c23cf49fb15b56b2b18027d33366974
sai-sreenath <sai-sreenath@users.noreply.github.com>
2020-04-15 11:41:43 -0400

    useState() hook for state manip in funct components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  
-  state={
-    persons: [
-      {name: 'Max', age:28},
-      {name: 'Manu', age:29},
-      {name: 'Stephanie', age:26}
-    ]
-  }
-  
-  switchNameHandler = () => {
+
+const App = props => {
+    
+  const [personsState, setPersonsState] = useState({
+      persons: [
+        {name: 'Max', age:28},
+        {name: 'Manu', age:29},
+        {name: 'Stephanie', age:26}
+      ],
+      otherState: 'Some other value'
+  });
+
+  const [otherState,setOtherState] = useState('Some Other Value');
+
+  const switchNameHandler = () => {
     //Shouldnt do this: this.state.persons[0].name='Maxmillan';
-    this.setState({
+    setPersonsState({
       persons: [
         {name: 'Maxmillan', age:28},
         {name: 'Manu', age:29},
         {name: 'Stephanie', age:27}
-      ]
+      ],
+     // otherState: personsState.otherState
     })
-  }
-
-  render() {
-    return (
+  };
+  
+  return (
       <div className="App">
         <h1>Hi, I'm a React app</h1>
         <p>This is really working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
       </div>
     );
   }
-}
+
 
 export default App;
+
+
+  
+  
